af8b3fde38e57c81f47eee9e78fb9755be8f5a4e
Kejni Koçi <59995634+kocikejni@users.noreply.github.com>
2024-03-18 23:24:03 +0100

    clientProvider

---
+++ app/actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs";
+import { StreamClient } from "@stream-io/node-sdk";
+import { error } from "console";
+
+export async function getToken() {
+  const streamApiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY;
+  const streamApiSecret = process.env.STREAM_VIDEO_API_SECRET;
+
+  if (!streamApiKey || !streamApiSecret) {
+    throw new Error("Stream Api key or secret not set");
+  }
+
+  const user = await currentUser();
+
+  console.log("Generating token for user: ", user?.id);
+
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const streamClient = new StreamClient(streamApiKey, streamApiKey);
+
+  const expirationTime = Math.floor(Date.now() / 100) + 60 * 60;
+
+  const issuedAt = Math.floor(Date.now() / 1000) - 600;
+
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
+
+  console.log("Token successfully created: ", token);
+
+  return token;
+}
