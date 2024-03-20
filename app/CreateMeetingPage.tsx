dce8450146c7390014a61871cc0016419dbcf010
Kejni Koçi <59995634+kocikejni@users.noreply.github.com>
2024-03-20 22:53:45 +0100

    CreateMettingPageButton

--- app/CreateMeetingPage.tsx
+++ app/CreateMeetingPage.tsx
@@ -1,17 +1,51 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { Loader2 } from "lucide-react";
+import { useState } from "react";
 
 export default function CreateMeetingPage() {
+  const client = useStreamVideoClient();
   const { user } = useUser();
 
-  if (!user) {
+  if (!client || !user) {
     return <Loader2 className="mx-auto animate-spin" />;
   }
   return (
     <div className="flex flex-col items-center space-y-6">
-      <h1>Welcome {user?.username}! </h1>
+      <h1 className="text-center text-2xl font-bold">
+        Welcome {user?.username}!{" "}
+      </h1>
+      <div className="mx-auto w-80 space-y-6 rounded-md bg-slate-100 p-5">
+        <h2 className="text-xl font-bold">Start a new meeting!</h2>
+        <DescriptionInput value="placeholder" onChange={() => {}} />
+      </div>
+    </div>
+  );
+}
+
+interface DescriptionInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+function DescriptionInput({ value, onChange }: DescriptionInputProps) {
+  const [active, setActive] = useState(false);
+  return (
+    <div className="space-y-2">
+      <div className="font-medium">Meeting Info:</div>
+      <label className="flex items-center gap-1.5">
+        <input
+          type="checkbox"
+          checked={active}
+          onChange={(e) => {
+            setActive(e.target.checked);
+            onChange("");
+          }}
+        />
+        Add description
+      </label>
     </div>
   );
 }
